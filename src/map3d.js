ef6c323c09543061717cfcd3f8560f691c7810cb
Chaoying <1754129@gmail.com>
2023-01-04 21:34:34 +0800

    feat: 车辆地形自适应批量实现

--- src/map3d.js
+++ src/map3d.js
@@ -5,6 +5,22 @@ import * as turf from "@turf/turf";
 import { getRotation } from "./utils";
 window.turf = turf;
 
+const getBoundingPoints = (lon, lat, dist) => {
+    const pointN = turf.rhumbDestination(turf.point([lon, lat]), dist, 0, {
+        units: "meters",
+    }).geometry.coordinates;
+    const pointE = turf.rhumbDestination(turf.point([lon, lat]), dist, 90, {
+        units: "meters",
+    }).geometry.coordinates;
+    const pointS = turf.rhumbDestination(turf.point([lon, lat]), dist, 180, {
+        units: "meters",
+    }).geometry.coordinates;
+    const pointW = turf.rhumbDestination(turf.point([lon, lat]), dist, -90, {
+        units: "meters",
+    }).geometry.coordinates;
+    return [pointE, pointS, pointW, pointN];
+};
+
 class Map3D {
     initMap(container) {
         Cesium.Ion.defaultAccessToken = CesiumToken;
@@ -39,7 +55,7 @@ class Map3D {
                 if (this.viewer.tracePoints.length === 0) {
                     this.viewer.tracePointsSource.entities.removeAll();
                     this.viewer.entities.removeById("traceLine");
-                    this.viewer.entities.removeById("car");     
+                    this.viewer.entities.removeById("car");
                     this.viewer.cancelTick && this.viewer.cancelTick();
                 }
                 const point = this.viewer.scene.pickPosition(
@@ -72,7 +88,11 @@ class Map3D {
                 let tracePointsDirections = [];
                 for (let i = 0; i < this.viewer.tracePoints.length; i++) {
                     if (i === this.viewer.tracePoints.length - 1) {
-                        tracePointsDirections.push(tracePointsDirections[tracePointsDirections.length - 1]);
+                        tracePointsDirections.push(
+                            tracePointsDirections[
+                                tracePointsDirections.length - 1
+                            ]
+                        );
                     } else {
                         tracePointsDirections.push(
                             Cesium.Cartesian3.normalize(
@@ -88,14 +108,23 @@ class Map3D {
                 }
                 this.viewer.clock.shouldAnimate = false;
                 const carPosition = new Cesium.SampledPositionProperty();
-                const carDirections = new Cesium.SampledProperty(Cesium.Cartesian3);
-                let start = Cesium.JulianDate.fromDate(new Date(), new Cesium.JulianDate());
+                const carDirections = new Cesium.SampledProperty(
+                    Cesium.Cartesian3
+                );
+                let start = Cesium.JulianDate.fromDate(
+                    new Date(),
+                    new Cesium.JulianDate()
+                );
                 this.viewer.clock.currentTime = start;
-                for (let i = 0; i < this.viewer.tracePoints.length; i++) { 
+                for (let i = 0; i < this.viewer.tracePoints.length; i++) {
                     const point = this.viewer.tracePoints[i];
                     carPosition.addSample(start, point);
                     carDirections.addSample(start, tracePointsDirections[i]);
-                    start = Cesium.JulianDate.addSeconds(start, 3, new Cesium.JulianDate());
+                    start = Cesium.JulianDate.addSeconds(
+                        start,
+                        3,
+                        new Cesium.JulianDate()
+                    );
                 }
                 this.viewer.clock.stopTime = Cesium.JulianDate.addSeconds(
                     start,
@@ -103,23 +132,74 @@ class Map3D {
                     new Cesium.JulianDate()
                 );
                 this.viewer.tracePoints = [];
-                this.viewer.entities.add({
+                this.x = new Cesium.CustomDataSource("xx");
+                this.viewer.dataSources.add(this.x);
+
+                this.x.entities.add({
                     id: "car",
-                    position:carPosition,
+                    position: carPosition,
+                    orientation:new Cesium.Quaternion(0,0,0,1),
                     model: {
                         uri: "car.gltf",
-                        scale: 5
-                    }
+                        scale: 5,
+                    },
                 });
-                this.viewer.cancelTick = this.viewer.clock.onTick.addEventListener(() => {
-                    const carEntity = this.viewer.entities.getById("car");
-                    const carPos = carEntity.position.getValue(this.viewer.clock.currentTime);
-                    const carDirection = carDirections.getValue(this.viewer.clock.currentTime);
-                    if (carPos) { 
-                        const carPos_ = this.viewer.scene.clampToHeight(carPos);
-                        
-                    }
-                }, this);
+                this.viewer.cancelTick =
+                    this.viewer.clock.onTick.addEventListener(() => {
+                        const carEntity = this.x.entities.getById("car");
+                        debugger;
+                        const carPos = carEntity.position.getValue(
+                            this.viewer.clock.currentTime
+                        );
+                        const carDirection = carDirections.getValue(
+                            this.viewer.clock.currentTime
+                        );
+                        if (carPos) {
+                            const carPos_ =
+                                this.viewer.scene.clampToHeight(carPos);
+                            const lon = Cesium.Math.toDegrees(
+                                Cesium.Cartographic.fromCartesian(carPos_)
+                                    .longitude
+                            );
+                            const lat = Cesium.Math.toDegrees(
+                                Cesium.Cartographic.fromCartesian(carPos_)
+                                    .latitude
+                            );
+                            const boundPoints = getBoundingPoints(lon, lat, 1.5);
+                            const boundPoints_ = boundPoints.map(point => {
+                                const point_ = Cesium.Cartesian3.fromDegrees(point[0], point[1]);
+                                const point__ = this.viewer.scene.clampToHeight(point_);
+                                return [point__.x, point__.y, point__.z];
+                            });
+                            const [a, b, c] = planeFit(boundPoints_);
+                            let zNormal = new Cesium.Cartesian3(a, b, c);
+                            let flag = Cesium.Cartesian3.dot(zNormal, carPos_);
+                            if (flag < 0) { 
+                                zNormal = new Cesium.Cartesian3(-a, -b,-c);
+                            }
+                            let yNormal = Cesium.Cartesian3.cross(zNormal, carDirection, new Cesium.Cartesian3());
+                            let xNormal = [
+                                carDirection.x,
+                                carDirection.y,
+                                carDirection.z,
+                            ];
+                            yNormal = [yNormal.x, yNormal.y, yNormal.z];
+                            zNormal = [zNormal.x, zNormal.y, zNormal.z];
+                            const rotationValues = getRotation(xNormal, yNormal, zNormal, [1, 0, 0], [0, 1, 0], [0, 0, 1]);
+                            const rotationMatirx = Cesium.Matrix3.fromArray(rotationValues);
+                            const rotationQua = Cesium.Quaternion.fromRotationMatrix(rotationMatirx, new Cesium.Quaternion());
+                            const scale = new Cesium.Cartesian3(
+                                carEntity.model.scale._value,
+                                carEntity.model.scale._value,
+                                carEntity.model.scale._value
+                            );
+                            const trs = new Cesium.TranslationRotationScale(carPos_, rotationQua, scale);
+                            const modelMatrix = Cesium.Matrix4.fromTranslationRotationScale(trs, new Cesium.Matrix4());
+                            if (carEntity.model._primitive) { 
+                                carEntity.model._primitive.modelMatrix = modelMatrix;
+                            }
+                        }
+                    }, this);
                 this.viewer.clock.shouldAnimate = true;
             },
             Cesium.ScreenSpaceEventType.RIGHT_CLICK,
