401d8c9d2aa630c5500814fd41e0d436c05f151d
Chaoying <1754129@gmail.com>
2023-02-15 21:54:52 +0800

    chore: 模型压平准备

--- src/map3d.js
+++ src/map3d.js
@@ -9,13 +9,18 @@ class Map3D {
 			animation: false,
 			shouldAnimate: true,
 		});
-		const flattenArea = Cesium.Cartesian3.fromDegreesArray([
+		const flattenAreaWC = Cesium.Cartesian3.fromDegreesArray([
 			-71.05546970246004, 42.35253974644013,
 			-71.0558516046633, 42.3517911739237,
 			-71.0574638361963, 42.3521936048791,
 			-71.05706149040155, 42.352934202982,
 		]);
-		
+		const modelMatrix = Cesium.Matrix4.IDENTITY;
+		const modelMatrixInv = Cesium.Matrix4.inverse(modelMatrix, new Cesium.Matrix4());
+		const flattenAreaMC = flattenAreaWC.map(point => {
+			return Cesium.Matrix4.multiplyByPoint(modelMatrixInv, point);
+		});
+		// 拟合模型平面
 		const tileset = this.viewer.scene.primitives.add(
 			new Cesium.Cesium3DTileset({
 				url: Cesium.IonResource.fromAssetId(354759),
