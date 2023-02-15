38c7c06f661687b3f6d7c4dd36c8a5c9d727e402
lcy <v-chaoying.lin@wz-inc.com>
2023-02-15 17:19:09 +0800

    feat: 倒车判断优化

--- src/map3d.js
+++ src/map3d.js
@@ -26,7 +26,6 @@ class Map3D {
 			console.log(Cesium.JulianDate.toDate(this.viewer.clock.currentTime));
 		}, 5000);
 		console.log("打印相关的信息");
-		return;
 		const carEntity = this.viewer.entities.add({
 			model: {
 				uri: "car3.gltf",
@@ -39,19 +38,18 @@ class Map3D {
 			),
 		});
 		this.viewer.zoomTo(carEntity);
-		return;
 		// this.viewer.camera.lookAt(
 		// 	Cesium.Cartesian3.fromDegrees(119.421871, 31.0362196),
 		// 	new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-15), 1000)
 		// );
-		this.viewer.camera.setView({
-			destination: Cesium.Cartesian3.fromDegrees(
-				119.406208,
-				31.025257,
-				1500
-			),
-			orientation: Cesium.HeadingPitchRoll.fromDegrees(45, -45, 0),
-		});
+		// this.viewer.camera.setView({
+		// 	destination: Cesium.Cartesian3.fromDegrees(
+		// 		119.406208,
+		// 		31.025257,
+		// 		1500
+		// 	),
+		// 	orientation: Cesium.HeadingPitchRoll.fromDegrees(45, -45, 0),
+		// });
 		this.interpolation();
 		this.viewer.clock.currentTime = Cesium.JulianDate.fromDate(
 			new Date(TracePoints.features[0].properties.t)
@@ -69,14 +67,14 @@ class Map3D {
 			for (let i = 0; i < TracePoints.features.length; i++) {
 				const t_ = TracePoints.features[i].properties.t;
 				if (t < t_) {
-					index = i;
+					index = i-1;
 					break;
 				}
 			}
 			if (!index) {
 				return;
 			}
-			const point_ = that.traceInterpolations[index - 1].getCoordinate(t);
+			const point_ = that.traceInterpolations[index].getCoordinate(t);
 			if (!point_) { 
 				return;
 			}
@@ -92,7 +90,7 @@ class Map3D {
 					coordinates: [point_.longitude, point_.latitude]
 				}
 			});
-			console.log("插值轨迹点", index, point_);
+			// console.log("插值轨迹点", index, point_);
 			interpolationPointDataSource.entities.add({
 				position: Cesium.Cartesian3.fromDegrees(
 					point_.longitude,
@@ -105,7 +103,7 @@ class Map3D {
 				},
 			});
 			
-			const point__ = that.traceInterpolations[index - 1].getCoordinate(
+			const point__ = that.traceInterpolations[index].getCoordinate(
 				t - 1
 			);
 			let heading = turf.bearing(
@@ -122,7 +120,6 @@ class Map3D {
 				}
 
 			}
-			console.log('倒车状态', this.carStstus[index]);
 			const position = Cesium.Cartesian3.fromDegrees(
 				point_.longitude,
 				point_.latitude,
@@ -144,45 +141,50 @@ class Map3D {
 		window.mathjs = mathjs;
 	}
 	interpolation() {
+		let flag = true;
 		this.carStstus = {};
 		this.traceInterpolations = [];
 		const points = TracePoints.features;
 		const num = points.length;
-		for (let i = 1; i < num; i++) {
-			this.carStstus[i] = true;
-			const point = { ...points[i] };
-			const lastPoint = { ...points[i - 1] };
-			let diffAngle = Math.abs(
-				point.properties.dir - lastPoint.properties.dir
-			);
-			diffAngle = diffAngle > 180 ? 360 - diffAngle : diffAngle;
-			if (diffAngle > 150) {
-				this.carStstus[i+1] = false;
-				point.properties.dir += 180;
-				if (point.properties.dir >= 360) {
-					point.properties.dir -= 360;
+		for (let i = 0; i < num - 1; i++) {
+			let currentPoint = points[i];
+			let nextPoint = points[i + 1];
+			if (i > 0) {
+				let lastPoint = points[i - 1];
+				let diffAngle = Math.abs(
+					currentPoint.properties.dir - lastPoint.properties.dir
+				);
+				diffAngle = diffAngle > 180 ? 360 - diffAngle : diffAngle;
+				if (diffAngle > 150) {
+					flag = !flag;
 				}
 			}
+			this.carStstus[i] = flag;
 			const start = {
-				t: lastPoint.properties.t,
-				latitude: lastPoint.geometry.coordinates[1],
-				longitude: lastPoint.geometry.coordinates[0],
-				speed: lastPoint.properties.speed / 3.6,
-				direction: lastPoint.properties.dir,
+				t: currentPoint.properties.t,
+				latitude: currentPoint.geometry.coordinates[1],
+				longitude: currentPoint.geometry.coordinates[0],
+				speed: currentPoint.properties.speed / 3.6,
+				direction: currentPoint.properties.dir,
 			};
 			const end = {
-				t: point.properties.t,
-				latitude: point.geometry.coordinates[1],
-				longitude: point.geometry.coordinates[0],
-				speed: point.properties.speed / 3.6,
-				direction: point.properties.dir,
+				t: nextPoint.properties.t,
+				latitude: nextPoint.geometry.coordinates[1],
+				longitude: nextPoint.geometry.coordinates[0],
+				speed: nextPoint.properties.speed / 3.6,
+				direction: nextPoint.properties.dir,
 			};
+			const diff = Math.abs(end.direction - start.direction);
+			if (diff > 150) { 
+				end.direction += 180;
+				end.direction = end.direction % 360;
+			}
 			let traceInterpolation = new TraceInterpolation(
 				start,
 				end,
 				TracePoints.features[0].properties.t
 			);
-			let point_ = traceInterpolation.getCoordinate(start.t);
+			// let point_ = traceInterpolation.getCoordinate(start.t);
 			// console.log(point_, start, end, "验证");
 			// console.log(
 			// 	traceInterpolation.lonFunc,
@@ -193,8 +195,8 @@ class Map3D {
 			this.traceInterpolations.push(traceInterpolation);
 			this.viewer.entities.add({
 				position: Cesium.Cartesian3.fromDegrees(
-					point.geometry.coordinates[0],
-					point.geometry.coordinates[1],
+					currentPoint.geometry.coordinates[0],
+					currentPoint.geometry.coordinates[1],
 					10
 				),
 				point: {
