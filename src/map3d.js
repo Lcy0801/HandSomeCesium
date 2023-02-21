a4a2529eec5f72ff3571097130a47b27b2ef4306
Chaoying <1754129@gmail.com>
2023-02-21 21:52:59 +0800

    feat: 初始化加载3dtiles

--- src/map3d.js
+++ src/map3d.js
@@ -7,8 +7,28 @@ class Map3D {
         this.viewer = new Cesium.Viewer(container, {
             timeline: false,
             animation:false,
-            shouldAnimate: true
-        });
-    }
+			shouldAnimate: true,
+			imageryProvider:new Cesium.OpenStreetMapImageryProvider()
+		});
+		window.viewer = this.viewer;
+		window.map3d = this;
+		window.Cesium = Cesium;
+		this.load3dTiles();
+		this.viewer.camera.setView({
+			destination: Cesium.Cartesian3.fromDegrees(
+				121.50355,
+				31.22690504445818,
+				1000
+			),
+			orientation: Cesium.HeadingPitchRoll.fromDegrees(0, -30, 0),
+		});
+	}
+	load3dTiles() { 
+		const tileset = this.viewer.scene.primitives.add(
+			new Cesium.Cesium3DTileset({
+				url: Cesium.IonResource.fromAssetId(96188),
+			})
+		);
+	}
 }
 export default new Map3D();
